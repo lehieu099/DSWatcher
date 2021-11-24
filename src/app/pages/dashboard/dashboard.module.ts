b1d10e750d97b21cc4ca5721e776e1099730b139
lehieu099 <duyhieu099a2@gmail.com>
2021-11-24 16:14:04 +0700

    icon

---
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+import { NgZorroAntdModule } from 'src/app/ng-antd-zorro/ng-zorro-antd.module';
+
+
+
+@NgModule({
+  declarations: [DashboardComponent],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    NgZorroAntdModule
+  ],
+  exports:[DashboardComponent]
+})
+export class DashboardModule { }
