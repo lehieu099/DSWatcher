424f7c333f343adc6804a21594d051ab4501a8b0
lehieu099 <duyhieu099a2@gmail.com>
2021-12-03 16:28:44 +0700

    dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
-import am5themes_Animated from "@amcharts/amcharts4/themes/Animated";
 
 @Component({
   selector: 'app-dashboard',
@@ -16,21 +15,21 @@ export class DashboardComponent implements OnInit {
 
     // Add dataF
     chart.data = [{
-      "country": "Lithuania",
+      "country": "Cá nhân",
       "litres": 501.9,
-      "color": am4core.color("#ED1C24")
+      "color": am4core.color("#1DC9B7")
     }, {
-      "country": "Czechia",
+      "country": "Fanpage",
       "litres": 301.9,
-      "color": am4core.color("#235789")
+      "color": am4core.color("#FFB822")
     }, {
-      "country": "Ireland",
+      "country": "Mobile",
       "litres": 201.1,
-      "color": am4core.color("#F1D302")
+      "color": am4core.color("#7371FC")
     }, {
-      "country": "Germany",
+      "country": "Bán hàng",
       "litres": 165.8,
-      "color": am4core.color("#020100")
+      "color": am4core.color("#B2967D")
     }];
 
     // Add and configure Series
@@ -40,6 +39,23 @@ export class DashboardComponent implements OnInit {
     pieSeries.slices.template.propertyFields.fill = "color";
 
     chart.legend = new am4charts.Legend();
+    pieSeries.labels.template.disabled = true;
+    pieSeries.ticks.template.disabled = true;
+    chart.legend.valueLabels.template.text = "";
+    chart.legend.useDefaultMarker = true;
+    // const marker:any = chart.legend.markers.template.children.getIndex(0);
+    // var marker:any = chart.legend.markers.template.children.getIndex(0);
+    // marker.cornerRadius(12, 12, 12, 12);
+
+
+    // marker.height=10;
+    // marker.width = 10;
+    chart.legend.position = "top";
+    // chart.legend.align="center";
+    chart.legend.fontSize = 12;
+    chart.align = "center";
+    chart.valign = "middle";
+    chart.legend.align ="center"
   }
 
   constructor() { }
