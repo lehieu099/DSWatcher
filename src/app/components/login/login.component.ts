e29fbdfad752c073393ee03e079430805085c59b
lehieu099 <duyhieu099a2@gmail.com>
2021-12-23 16:27:07 +0700

    login components

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private fb: FormBuilder, private router: Router) { }
+
+  validateForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: ['', [Validators.required]],
+      passWord: ['', [Validators.required]],
+      remember: [true]
+    });
+  }
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      if (this.validateForm.controls.hasOwnProperty(i)) {
+        this.validateForm.controls[i].markAsDirty();
+        this.validateForm.controls[i].updateValueAndValidity();
+      }
+    }
+    if (this.validateForm.valid) {
+      this.router.navigate(['/dashboard'])
+      console.log(this.validateForm.value);
+    }
+  }
+}
