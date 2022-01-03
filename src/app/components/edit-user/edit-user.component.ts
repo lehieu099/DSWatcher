7f81c20479bbe9730c8f39d21036b0b1bab30c55
lehieu099 <duyhieu099a2@gmail.com>
2022-01-03 19:03:14 +0700

    change variable name

--- src/app/components/edit-user/edit-user.component.ts
+++ src/app/components/edit-user/edit-user.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NzMessageService } from 'ng-zorro-antd/message';
-import { DSwatcherService } from 'src/app/service/dswatcher.service';
+import { DSwatcherService } from 'src/app/service/user.service';
 
 
 @Component({
@@ -14,31 +14,41 @@ export class EditUserComponent implements OnInit {
 
   editUserForm: FormGroup;
   currentUser: any;
+  username: string = "";
+  permission: string = "";
+  status: boolean = false;
+  email: string = "";
 
   constructor(private router: Router,
     private message: NzMessageService,
     private fb: FormBuilder,
     private dsWatcherService: DSwatcherService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private zone: NgZone
   ) {
     this.editUserForm = this.fb.group({
-      userNameEdit: ['', [Validators.required]],
-      perMissionEdit: [''],
+      usernameEdit: ['', [Validators.required]],
+      permissionEdit: [''],
       statusEdit: [''],
       emailEdit: ['', [Validators.required, Validators.email]]
     })
   }
 
   ngOnInit(): void {
-    this.getUser(this.route.snapshot.paramMap.get('id'));
+    this.zone.run(() => {
+      this.getUser(this.route.snapshot.paramMap.get('id'));
+    })
   }
 
   getUser(id: any) {
     this.dsWatcherService.get(id).subscribe(
       data => {
         this.currentUser = data;
+        this.username = this.currentUser.username;
+        this.permission = this.currentUser.permission;
+        this.status = this.currentUser.status;
+        this.email = this.currentUser.email;
         console.log(data);
-
       },
       error => {
         console.log(error);
@@ -51,7 +61,7 @@ export class EditUserComponent implements OnInit {
       data => {
         console.log(data);
         this.message.create('success', `Cập nhật người dùng thành công!`);
-        this.router.navigateByUrl('/user');
+        this.router.navigateByUrl('/users');
       },
       error => {
         console.log(error);
@@ -60,6 +70,6 @@ export class EditUserComponent implements OnInit {
   }
 
   cancel() {
-    this.router.navigateByUrl('/user');
+    this.router.navigateByUrl('/users');
   }
 }
